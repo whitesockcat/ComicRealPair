4baf31c4459072c0620937c031fd4ae9f3877565
guo <robin_gary@foxmail.com>
2019-04-16 11:51:50 +0800

    Update comic_real_pair.py

--- comic_real_pair.py
+++ comic_real_pair.py
@@ -1,81 +1,55 @@
-
-
+import random
+from PIL import Image
+import torch
+import os
+from torch.utils.data import DataLoader,Dataset
+import numpy as np
 
 class ComicRealPairDataset(Dataset):
     
     def __init__(self,
-                data_dir,
+                pic_dir,
                 ann_dir,
                 transform=None):
-        self.data_dir = data_dir  
+        self.pic_dir = pic_dir 
         self.ann_dir = ann_dir  
         self.transform = transform
-        self.ids = # TODO
+        comic_num = 100
+        real_num = 150
+        # 选一个数量多的
+        self.ids = [i for i in range(real_num)]
 
-    def _get_path(self, path0, IsSame = True):
-        path = None
-        if IsSame:
-            # TODO
-        else:
-            # TODO
-        return path
+    def _get_path1(self, idx, isSame = True):
+        path1 = None
+        return path1
+
+    def _get_path0(self, idx):
+        name = 'abc.jpg' # TODO
+        path0 = self.pic_dir + name
+        return path0
+
+    def _get_target(self, isSame):
+        target = torch.from_numpy(np.array([isSame])) # TODO
+        return target
 
     def __getitem__(self,index):
         img_id = self.ids[index]
+        is_same_class = random.randint(0,1)
+
+        path0 = self._get_path0(img_id)
+        path1 = self._get_path1(img_id, is_same_class)
+
+        img0 = Image.open(path0).convert('RGB')
+        img1 = Image.open(path1).convert('RGB')
         
         if self.transform is not None:
             img0 = self.transform(img0)
             img1 = self.transform(img1)
         
-        return img0, img1 , torch.from_numpy(np.array([int(img1_tuple[1]!=img0_tuple[1])],dtype=np.float32))
+        target = self._get_target(is_same_class)
+
+        return img0, img1, target
     
     def __len__(self):
-        return len(self.imageFolderDataset.imgs)
-
-
-class CocoDetection(data.Dataset):
-    """`MS Coco Detection <http://mscoco.org/dataset/#detections-challenge2016>`_ Dataset.
-
-    Args:
-        root (string): Root directory where images are downloaded to.
-        annFile (string): Path to json annotation file.
-        transform (callable, optional): A function/transform that  takes in an PIL image
-            and returns a transformed version. E.g, ``transforms.ToTensor``
-        target_transform (callable, optional): A function/transform that takes in the
-            target and transforms it.
-    """
-
-    def __init__(self, root, annFile, transform=None, target_transform=None):
-        from pycocotools.coco import COCO
-        self.root = root
-        self.coco = COCO(annFile)
-        self.ids = list(self.coco.imgs.keys())
-        self.transform = transform
-        self.target_transform = target_transform
-
-    def __getitem__(self, index):
-        """
-        Args:
-            index (int): Index
-
-        Returns:
-            tuple: Tuple (image, target). target is the object returned by ``coco.loadAnns``.
-        """
-        coco = self.coco
-        img_id = self.ids[index]
-        ann_ids = coco.getAnnIds(imgIds=img_id)
-        target = coco.loadAnns(ann_ids)
-
-        path = coco.loadImgs(img_id)[0]['file_name']
-
-        img = Image.open(os.path.join(self.root, path)).convert('RGB')
-        if self.transform is not None:
-            img = self.transform(img)
-
-        if self.target_transform is not None:
-            target = self.target_transform(target)
-
-        return img, target
-
-    def __len__(self):
         return len(self.ids)
+
